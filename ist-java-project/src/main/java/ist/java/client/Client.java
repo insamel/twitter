7bcfa2384261545d2dcb21b443c6ffc991705004
Jes <kontaktjes@gmail.com>
2018-11-09 13:26:40 +0100

    Working ServerBlog and Client added.

--- ist-java-project/src/main/java/ist/java/client/Client.java
+++ ist-java-project/src/main/java/ist/java/client/Client.java
@@ -3,35 +3,90 @@ package ist.java.client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.*;
+import java.net.*;
 
 public class Client {
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private Socket connection;
+
+	// Empty constructor for now
+	public Client(){
+	}
+
     public static void main(String... args) throws IOException{
 
-                
-                System.out.println("1: Write a tweet");
-                System.out.println("2: Read all tweets");
-                System.out.println("x: Exit");
-                System.out.println("What do you want to do?");
-                String action = System.in.read(); //if else for other letters
+		System.out.println("1: Write a tweet");
+        System.out.println("2: Read all tweets");
+        System.out.println("x: Exit");
+        System.out.println("What do you want to do?");
+        String action = System.in.read(); //if else for other letters
 
-                if(action == 1){
-                System.out.println("Please enter your username:");
-                System.out.println();
-                String author = System.in.read(); 
+        if(action == 1){
+	        System.out.println("Please enter your username:");
+            System.out.println();
+            String author = System.in.read(); 
 
-                System.out.println("What do you want to talk about ? (120 characters)");
-                System.out.println();   	
-                String tweet = System.in.read(); 
+            System.out.println("What do you want to talk about ? (120 characters)");
+            System.out.println();   	
+            String tweet = System.in.read(); 
 
-                System.out.println("send post: ");
-                System.out.println("timestamp " + );
-                System.out.println("author: \"" + author +"\"");
-                System.out.println("tweet: \"" + tweet +"\"");
+            System.out.println("send post: ");
+            System.out.println("timestamp " );
+            System.out.println("author: \"" + author +"\"");
+            System.out.println("tweet: \"" + tweet +"\"");
 
-                //wrapping in a PostSubmission class before sending to the network
-                } else{
-                    //handling if it is a request
-                }
+            //wrapping in a PostSubmission class before sending to the network
+            } else{
+            	//handling if it is a request
+	    }
 
-    }
+
+		// Client handling code from here and down
+		Client client = new Client(); 
+		while(true){  
+			client.startRunning();
+		}
+    } // end of main
+
+	//connect to server
+	public void startRunning(){
+		try{
+			connectToServer();
+			streamSetup();
+		}catch(EOFException eofException){
+			System.out.println("\n Client terminated the connection");
+		}catch(IOException ioException){
+			ioException.printStackTrace();
+		}finally{
+			close();
+		}
+	}
+
+	//connect to server
+	private void connectToServer() throws IOException{
+		connection = new Socket("localhost", 4040); 
+		System.out.println("Connected to localhost in port 4040");
+		// Connects on localhost (IP that always route back to our own computer) and port 4040. 
+	}
+
+	//setup of IO streams
+	private void streamSetup() throws IOException{
+		output = new ObjectOutputStream(connection.getOutputStream());
+		output.flush();
+		input = new ObjectInputStream(connection.getInputStream());
+	//	showMessage("\n The streams are now set up! \n");
+	}
+
+	//Close connection
+	private void close(){
+		try{
+			output.close();
+			input.close();
+			connection.close();
+		}catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+	}	
 }
--- ist-java-project/src/main/java/ist/java/server/BlogServer.java
+++ ist-java-project/src/main/java/ist/java/server/BlogServer.java
@@ -1,12 +1,60 @@
 package ist.java.server;
 
 import ist.java.data.Blog;
+import java.io.*;
+import java.net.*;
 
 public class BlogServer {
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private ServerSocket server;
+	private Socket connection;
 
-    private static Blog blog = new Blog();
+	// constructor doing nothing at the moment
+	public void BlogServer(){
+	}
 
     public static void main(String... args){
+		BlogServer newServer = new BlogServer();			// Creates an object 'newServer', which will later hold all connections (?)
+		newServer.startRunning();							// Start running 'server'. 
+	}	
+
+	// setup of server
+	public void startRunning(){
+		try{
+			server = new ServerSocket(4040, 100);		// portnumber, 100 connection can wait at the port (called backlog)
+			while(true){								// look forever for new connections
+				try{
+					connection = server.accept();	 	// accepts if connection is possible and creates socket
+					streamSetup();					
+				}catch(EOFException eofException){		// when someone ends the connection, the following message will be displayed. 
+					System.out.print("Connection ended.");
+				}finally{
+					close();							// Method to close output, input and connection. 
+				}
+			}
+		}catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+	}
+
+	// Get stream to send and receive data
+	private void streamSetup() throws IOException{
+		output = new ObjectOutputStream(connection.getOutputStream());	// creates object 'ObjectOutputStream'. From documentation about getOutputStream(): This method returns an OutputStream where the data can be written. 
+		output.flush();													// cleans the stream, if all bytes were not correctly send. 
+		input = new ObjectInputStream(connection.getInputStream());		// creates object 'ObjectInputStream'. From documentation about getInputStream(): This method returns an InputStream representing the data. 
+	}
+
+	// to close streams and sockets when connection is ended
+	private void close(){
+		try{
+			output.close();							// closes stream i&o
+			input.close();
+			connection.close();						// closes socket to not waste memory
+		}catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+
         //should listen to port 4040
 
         //wait for incoming connection
@@ -25,8 +73,6 @@ public class BlogServer {
     	//}
 
         //wait for another incoming connection
-
-        
 
     }
 }
