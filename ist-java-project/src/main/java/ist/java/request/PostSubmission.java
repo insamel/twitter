18fce89ba3ccbcfc650d7107c52546db8aef8f79
irenearroyo <44394678+irenearroyo@users.noreply.github.com>
2018-11-12 17:55:49 +0100

    post submission

--- ist-java-project/src/main/java/ist/java/request/PostSubmission.java
+++ ist-java-project/src/main/java/ist/java/request/PostSubmission.java
@@ -1,4 +1,12 @@
 package ist.java.request;
 
 public class PostSubmission {
+   private String author;
+    private String tweet;
+    private Date timestamp;
+    public PostSubmission( String author,String tweet,Date timestamp){
+        this.author=author;
+        this.tweet=tweet;
+        this.timestamp=timestamp;
+
 }
